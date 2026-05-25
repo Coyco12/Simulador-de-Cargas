3b72492e2cda6948189d20f3e8b5e7ba1ea35548
Coyco12 <santicoy2906@gmail.com>
2026-05-25 00:42:21 -0600

    V2 25 05 12:42

--- canvas.js
+++ canvas.js
@@ -3,20 +3,21 @@ let cuadrox = document.getElementById("numbx");
 
 let canvas=document.getElementById("canvas");
 let figura=canvas.getContext("2d");
-
+let container= canvas.parentNode;
 
 var windowWidth=window.innerWidth;
 var windowHeight=window.innerHeight;
-canvas.width=windowWidth;
-canvas.height=windowHeight;
+canvas.width=container.clientWidth-20;
+canvas.height=container.clientHeight-20;
 
 
-class circle{
-    constructor(x,y,radio,color) {
+class carga{
+    constructor(x,y,radio,color,valor){
         this.x=x; 
         this.y=y;
         this.radio=radio;
         this.color=color;
+        this.valor=valor;
     }
     draw(){
         figura.beginPath();
@@ -24,13 +25,11 @@ class circle{
         figura.fillStyle=this.color;
         figura.fill();
         figura.strokeStyle="black";
-        figura.lineWidth=5;
+        figura.lineWidth=2;
         figura.stroke();
-        
-    
         figura.textAlign="center";
         figura.textBaseline="middle";
-        figura.font="30px Arial";
+        figura.font="20px Arial";
         figura.fillStyle="white";
         if(this.color=="darkblue"){
             figura.fillText("-",this.x,this.y);
@@ -41,13 +40,17 @@ class circle{
     }
 }
 
-botton.addEventListener("click", function() {
-    let valx= Number(cuadrox.value);
-    let circles= new circle(valx,200,50,"red");
-    circles.draw();
-});
+canvas.addEventListener("click", createCharge);
 
-
+function createCharge(event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    let allcargas = [];
+    let newCharge = new carga(x, y, 25, "red");
+    newCharge.draw();
+    allcargas.push(newCharge);
+}
 
 
 
