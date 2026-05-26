17be199bc1479c3e6f49404d7fc9ff83d2f91a9e
Coyco12 <santicoy2906@gmail.com>
2026-05-26 14:43:45 -0600

    V6 26 05 2:43
    Lineas de fuerza, falta calcular su suma
    botón borrar switch

--- canvas.js
+++ canvas.js
@@ -4,8 +4,9 @@ let dimension0= 'Selecciona una dimensión para colocar las cargas';
 let cuadro1, dim, canvas, figura, container;
 let switch_click = "crear";
 var allcargas = [];
-
-
+let button_borrar = document.getElementById("borrar");
+button_borrar.classList.remove("borrando");
+let prefix = document.getElementById("prefix");
 function inicializarYReseize() {
     
     cuadro1 = document.getElementById("valorcarga");
@@ -35,6 +36,14 @@ window.addEventListener("DOMContentLoaded", inicializarYReseize);
 
 window.addEventListener("resize", inicializarYReseize);
 
+button_borrar.addEventListener("click",()=> {
+    button_borrar.classList.toggle("borrando");
+});
+
+function calcDistancia(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
 class carga{
     constructor(x,y,radio,valor){
         this.x=x; 
@@ -55,13 +64,7 @@ class carga{
         if(this.y>canvas.height-this.radio){
             this.y=canvas.height-this.radio;
         }
-        /*
-        for(each of allcargas){
-            if(each!=this&&this.x+this.radio<each.x-each.radio&&each.x-this.x<this.radio+each.radio){
-                this.x-=Math.cos(Math.atan((each.y-this.y)/(each.x-this.x)))*this.radio;
-            }
-        }
-        */
+       
         figura.beginPath();
         figura.arc(this.x,this.y,this.radio,0,Math.PI*2);
         if(this.valor<0){
@@ -88,8 +91,6 @@ class carga{
     }
 }
 
-
-
 function cambiodim(event){
     if(dim.value=="1D"||dim.value=="2D"||dim.value=="general"){
         limpiar();
@@ -98,10 +99,59 @@ function cambiodim(event){
 
 }
 function click_switch(event){
-    if(switch_click=="borrar")
+    if(button_borrar.classList.contains("borrando"))
         borrar(event);
-    else 
+    else
         createCharge(event);
+}
+
+function measureCharge(event){
+    let rect = canvas.getBoundingClientRect();
+    let x_clic = event.clientX - rect.left;
+    let y_clic = event.clientY - rect.top;
+    let mindistance = Infinity;
+    let closestcharge = null;
+    for (let i = 0; i < allcargas.length; i++) {
+        if(allcargas[i]!=closestcharge){
+            let cargaActual = allcargas[i];
+            let distancia = calcDistancia(cargaActual.x,cargaActual.y,x_clic,y_clic);
+            if (distancia <= 1.75*cargaActual.radio) {
+                if (distancia < mindistance){
+                    mindistance = distancia;
+                    closestcharge = cargaActual;
+                }
+            }
+        }
+    }
+    if(closestcharge==null){
+        return false;
+    }else{
+        for (let i = 0; i < allcargas.length; i++) {
+            if(allcargas[i]!=closestcharge){
+                let cargaActual = allcargas[i];
+                let distancia = calcDistancia(cargaActual.x,cargaActual.y,x_clic,y_clic);
+                let fuerza= (9*Math.pow(10,9)*cargaActual.valor*closestcharge.valor)/Math.pow(distancia,2);
+                figura.beginPath();
+                figura.moveTo(closestcharge.x, closestcharge.y);
+                figura.lineTo(cargaActual.x, cargaActual.y);
+                figura.strokeStyle="blue";
+                figura.lineWidth=2;
+                figura.stroke();
+                figura.save();
+                figura.translate((closestcharge.x+cargaActual.x)/2, (closestcharge.y+cargaActual.y)/2);
+                let angle = Math.atan2(cargaActual.y - closestcharge.y, cargaActual.x - closestcharge.x);
+                figura.rotate(angle);
+                figura.font="20px Arial";
+                figura.fillStyle="white";
+                figura.textAlign="center";
+                figura.textBaseline="middle";
+                figura.fillText(fuerza.toExponential(2)+" N", (closestcharge.x+cargaActual.x)/2, (closestcharge.y+cargaActual.y)/2);
+                figura.restore();
+                figura.closePath();
+            }
+        }
+        return true;
+    }
 }
 
 function createCharge(event) {   
@@ -115,18 +165,27 @@ function createCharge(event) {
     if(dim.value=="general"){
         notificacion(dimension0);
         return;
-    }    
-    let coulomb=Number(cuadro1.value);
-    if(coulomb==0){
-        notificacion(carga0);
-        return;
-    }  
-    let newCharge = new carga(x, y, 25, coulomb);
-    newCharge.draw();
-    allcargas.push(newCharge);
-    
+    }
+    if(!measureCharge(event)){    
+        let coulomb=Number(cuadro1.value);
+        let prefixValue = prefix.value;
+        if (prefixValue === "milicoulomb") {
+            coulomb *= 1e-3;
+        } else if (prefixValue === "microcoulomb") {
+            coulomb *= 1e-6;
+        }
+        else if (prefixValue === "nanocoulomb") {
+            coulomb *= 1e-9;
+        }
+        if(coulomb==0){
+            notificacion(carga0);
+            return;
+        }  
+        let newCharge = new carga(x, y, 25, coulomb);
+        allcargas.push(newCharge);
+        newCharge.draw();
+    }
 }
-
 function borrar(event){
     let rect = canvas.getBoundingClientRect();
     let x_clic = event.clientX - rect.left;
@@ -134,11 +193,7 @@ function borrar(event){
 
     for (let i = 0; i < allcargas.length; i++) {
         let cargaActual = allcargas[i];
-
-        let distanciaX = x_clic - cargaActual.x;
-        let distanciaY = y_clic - cargaActual.y;
-        let distancia = Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
-
+        let distancia = calcDistancia(cargaActual.x,cargaActual.y,x_clic,y_clic);
         if (distancia <= cargaActual.radio) {
             allcargas.splice(i, 1); 
             figura.clearRect(0, 0, canvas.width, canvas.height);
